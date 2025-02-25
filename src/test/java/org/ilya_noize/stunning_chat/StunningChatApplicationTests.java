a0f771b9462111e88e5a38f65652cb6d21713948
ilya-noize <shumakoff.ilya@yandex.ru>
2025-02-25 13:29:36 +0300

    starring maven, spring;

---
+++ src/test/java/org/ilya_noize/stunning_chat/StunningChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilya_noize.stunning_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StunningChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
